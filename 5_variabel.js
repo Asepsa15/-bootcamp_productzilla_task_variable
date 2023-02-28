c9120828af2b2de4317c4df777b1256596f1f8cb
Asepsa15 <asepsaripudin2002@gmail.com>
2023-02-28 22:07:22 +0700

    tugas satu

---
+++ 5_variabel.js
@@ -0,0 +1,15 @@
+console.log('operator antarmatika')
+const angka1 = 10
+const angka2 = 10
+const angka3 = 10
+let penjumlahan = angka1 + angka2 - angka3
+let pengurangan = angka1 - angka2 + angka3
+let perkalian = angka1 * angka2 ** angka3
+let perpangkatan = angka1 ** angka2 / angka3
+let pembagian = angka1 / angka2 * angka3
+
+console.log(penjumlahan)
+console.log(pengurangan)
+console.log(perkalian)
+console.log(perpangkatan)
+console.log(pembagian)
---
+++ operator-perbandingan.js
@@ -0,0 +1,39 @@
+console.log('operator perbandigan')
+const angka1 = 10
+const angka2 = 5
+const angka3 = 10
+const angka4 = 5
+let perbandingan = ''
+
+console.log(angka1,'>',angka2 , angka1 > angka2);
+perbandingan = angka1 > angka2
+console.log(angka3,'>',angka4 , angka3 > angka4);
+perbandingan = angka3 > angka4
+console.log(angka1,'<',angka2 , angka1 < angka2);
+perbandingan = angka1 < angka2
+console.log(angka3,'<',angka4 , angka3 < angka4);
+perbandingan = angka3 < angka4
+console.log(angka1,'>=',angka2 , angka1 >= angka2);
+perbandingan = angka1 >= angka2
+console.log(angka3,'>=',angka4 , angka3 >= angka4);
+perbandingan = angka3 >= angka4
+console.log(angka1,'<=',angka2 , angka1 <= angka2);
+perbandingan = angka1 <= angka2
+console.log(angka3,'<=',angka4 , angka3 <= angka4);
+perbandingan = angka3 <= angka4
+console.log(angka1,'==',angka2 , angka1 == angka2)
+perbandingan = angka1 == angka2
+console.log(angka3,'==',angka4 , angka3 == angka4)
+perbandingan = angka3 == angka3
+console.log(angka1,'===',angka2 , angka1 === angka2)
+perbandingan = angka1 === angka2
+console.log(angka3,'===',angka4 , angka3 === angka4)
+perbandingan = angka3 === angka4
+console.log(angka1,'!=',angka2 , angka1 != angka2);
+perbandingan = angka1 != angka2
+console.log(angka3,'!=',angka4 , angka3 != angka4);
+perbandingan = angka3 != angka4
+console.log(angka1,'!==',angka2 , angka1 !== angka2);
+perbandingan = angka1 !== angka2
+console.log(angka3,'!==',angka4 , angka3 !== angka4);
+perbandingan = angka3 !== angka4
